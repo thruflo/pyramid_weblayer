3e104f018b9b0212350a15f80563a3a0af5cd8a6
James Arthur <thruflo@gmail.com>
2013-09-13 18:22:40 +0100

    patch: horribly hardcoded dogpile integration.

--- src/pyramid_weblayer/patch.py
+++ src/pyramid_weblayer/patch.py
@@ -11,6 +11,85 @@ from beaker import cache
 from beaker.util import coerce_cache_params
 from pyramid import mako_templating
 
+from mako.cache import register_plugin
+from mako.lookup import TemplateLookup
+
+try:
+    from dogpile.cache import make_region
+    from dogpile.cache.plugins.mako_cache import MakoPlugin
+except ImportError:
+    pass
+else:
+    class UnicodeKeyCapableMakoPlugin(MakoPlugin):
+        """Override the plugin to handle unicode keys gracefully."""
+        
+        def get_and_replace(self, key, *args, **kw):
+            if isinstance(key, unicode):
+                key = key.encode('utf-8')
+            return MakoPlugin.get_and_replace(self, key, *args, **kw)
+
+        def get_or_create(self, key, *args, **kw):
+            if isinstance(key, unicode):
+                key = key.encode('utf-8')
+            return MakoPlugin.get_or_create(self, key, *args, **kw)
+
+        def put(self, key, *args, **kw):
+            if isinstance(key, unicode):
+                key = key.encode('utf-8')
+            return MakoPlugin.put(self, key, *args, **kw)
+
+        def get(self, key, *args, **kw):
+            if isinstance(key, unicode):
+                key = key.encode('utf-8')
+            return MakoPlugin.get(self, key, *args, **kw)
+
+        def invalidate(self, key, *args, **kw):
+            if isinstance(key, unicode):
+                key = key.encode('utf-8')
+            return MakoPlugin.invalidate(self, key, *args, **kw)
+        
+    
+
+def coerge_dogpile_args(args):
+    """Coerce INI settings into cache args with a ``default`` dogpile region.
+      
+      Requires args having at least:
+      - url
+      - type
+      - expire
+      
+      XXX replace this as and when you actually want multiple regions.
+    """
+    
+    # If the url isn't a list, make it so.
+    url = args['url']
+    if isinstance(url, basestring):
+        url = [url]
+    
+    # Build the backend args.
+    backend = args['type']
+    backend_kwargs = {
+        'url': [args['url']],
+    }
+    expiration_time = args['expire']
+    
+    # With optional auth.
+    username = args.get('username', None)
+    if username is not None:
+        backend_kwargs['username'] = username
+    password = args.get('password', None)
+    if password is not None:
+        backend_kwargs['password'] = password
+    
+    # Make the mako cache happy dogpile region.
+    region = make_region().configure(
+        backend,
+        expiration_time=expiration_time,
+        arguments=args
+    )
+    return {'regions': {'default': region}}
+
+    
 def templateLookupFactory(settings):
     """Use the ``settings`` to return a patched ``TemplateLookup`` class."""
     
@@ -35,6 +114,11 @@ def templateLookupFactory(settings):
                         cache_args[name] = value
                 coerce_cache_params(cache_args)
                 cache_args['timeout'] = cache_args.get('expire')
+                if cache_args['type'].startswith('dogpile.cache'):
+                    cache_args = coerge_dogpile_args(cache_args)
+                    register_plugin("dogpile.cache",
+                            "pyramid_weblayer.patch", "UnicodeKeyCapableMakoPlugin")
+                    kwargs['cache_impl'] = 'dogpile.cache'
                 kwargs['cache_args'] = cache_args
             super(TemplateLookup, self).__init__(*args, **kwargs)
         
