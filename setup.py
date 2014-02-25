9c175a011b285f5decb5e9757461dd059a815f4e
James Arthur <thruflo@gmail.com>
2014-02-25 10:37:18 +0000

    release: v0.12.2.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'pyramid_weblayer',
-    version = '0.12.1',
+    version = '0.12.2',
     description = 'Common / reusable utilities for a Pyramid web application.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
