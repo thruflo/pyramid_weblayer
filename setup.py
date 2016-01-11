e4cfeb19a1070fe858da7ded5b9a73acaeb01221
andrecp <andreprado88@gmail.com>
2016-01-11 17:27:33 +0000

    request logger: now logging 5xx requests as well.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'pyramid_weblayer',
-    version = '0.14.5',
+    version = '0.14.6',
     description = 'Common / reusable utilities for a Pyramid web application.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
